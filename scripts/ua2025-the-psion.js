d972c8b46ba8123c98d0aee4c4b89fe6e17123d9
Henry Malinowski <henry-malinowski@users.noreply.github.com>
2025-06-15 16:06:17 -0500

    v1.0.1

--- scripts/ua2025-the-psion.js
+++ scripts/ua2025-the-psion.js
@@ -1,6 +1,5 @@
 Hooks.once("init", () => {
-  console.log("psion_mods.js hooked");
+  console.log("ua2025-the-psion.js hooked");
   CONFIG.DND5E.featureTypes.class.subtypes.psionicDiscipline = "Psionic Discipline";
   CONFIG.DND5E.featureTypes.feat.subtypes.wildTalentFeat = "Wild Talent Feat";
-
 });
