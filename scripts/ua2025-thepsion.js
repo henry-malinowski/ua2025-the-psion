ef46a41cfcdd028b035256fa446ff1cf491e0b10
Henry Malinowski <henry-malinowski@users.noreply.github.com>
2025-06-15 16:51:41 -0500

    Overwrite main's contents with bv1.0.1

--- scripts/ua2025-thepsion.js
+++
@@ -1,6 +0,0 @@
-Hooks.once("init", () => {
-  console.log("psion_mods.js hooked");
-  CONFIG.DND5E.featureTypes.class.subtypes.psionicDiscipline = "Psionic Discipline";
-  CONFIG.DND5E.featureTypes.feat.subtypes.wildTalentFeat = "Wild Talent Feat";
-
-});
