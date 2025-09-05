263f431e06a0b6963e5aca02493ac7e00d2f41bb
Henry Malinowski <henry-malinowski@users.noreply.github.com>
2025-09-05 12:09:07 -0500

    Minor improvements and cleanup
    * Added new custom class and subclass icons to match general style used by FoundryVTT
    * Added example art to the top of the Psion class Journal
    * Added banner art to the Psion compendium packs.

--- scripts/ua2025-the-psion.js
+++ scripts/ua2025-the-psion.js
@@ -1,5 +1,5 @@
 Hooks.once("init", () => {
-  console.log("ua2025-the-psion.js hooked");
   CONFIG.DND5E.featureTypes.class.subtypes.psionicDiscipline = "Psionic Discipline";
   CONFIG.DND5E.featureTypes.feat.subtypes.wildTalentFeat = "Wild Talent Feat";
+  console.log("ua2025-the-psion.js hooked");
 });
